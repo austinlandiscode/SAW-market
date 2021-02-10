0cec410699b2be2487174ed276860aa1ca8dcdcb
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-10 15:52:24 -0800

    (end of day 1)
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,15 +6,57 @@ import MarketService from './market.js';
 
 let promise = MarketService.sortCoin();
 promise.then(function(response) {
-  
   const body = JSON.parse(response);
-  console.log(Object.entries(body));
-  for (const [key, value] of Object.entries(body)){
-    const nm = key.toUpperCase();
-    const price = JSON.stringify(value.usd);
-    const change= JSON.stringify(value.usd_24h_change);
-    const vol= JSON.stringify(value.usd_24h_vol);
-    const cap= JSON.stringify(value.usd_market_cap);
-    $("#sorted").append(`<p>${nm} Price: $${price} | 24Hour Change:$${change} | Volume:$${vol} | Market Cap:$${cap} <br></p>`);
-  }
+  let sorted = Object.entries(body).sort(function(a,b){
+    if (a[1].usd < b[1].usd) return 1;
+    if (a[1].usd > b[1].usd) return -1;
+    return 0;
+  });
+
+  displayData(sorted);
+  $("#priceTable>thead>tr>th").click(function () {
+    console.log($(this).attr("id"));
+    // Object.entries(body).sort(function (a, b) {  
+  //     if ($(this).attr("id") === "ID"){
+  //   if (a[0] < b[0]) return 1;
+  //   if (a[0] > b[0]) return -1;
+  //   return 0;
+  // }
+  //   else{
+  //   if (a[1].usd < b[1].usd) return 1;
+  //   if (a[1].usd > b[1].usd) return -1;
+  //   return 0;
+  //   }
+  // })
+  });
 });
+
+function displayData(array)
+{
+  for (const [key, value] of array){
+    const nm = key.toUpperCase();
+    const price = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value.usd);
+    const change = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value.usd_24h_change);
+    const vol = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value.usd_24h_vol);
+    const cap = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value.usd_market_cap);
+    $("#sorted").append(`<tr><td>${nm}</td> <td>${price}</td>  <td>${change}</td> <td>${vol}</td> <td>${cap}</td></tr> <br>`);
+  }
+}
+/*
+$('#refresh').on('click', function() {
+  
+});
+
+
+$("#priceTable>thead>tr>th").click(function(){
+  
+  Object.entries(body).sort(function(a,b){
+    if($(this).attr("id")==="ID"))
+      if (a[0] < b[0]) return 1;
+      if (a[0] > b[0]) return -1;
+      return 0;
+    else
+      if (a[1].usd < b[1].usd) return 1;
+      if (a[1].usd > b[1].usd) return -1;
+      return 0;
+}) */
--- src/market.js
+++ src/market.js
@@ -2,7 +2,7 @@ export default class MarketService {
   static sortCoin() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Clitecoin%2Cethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
+      const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cnamecoin%2Cripple%2Calgorand%2Ctezos%2Cdecentraland%2Cdogecoin%2Clitecoin%2Ccardano%2Cethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);

a31fe684278a5fad39b1ddc4e74f9e8bc9518ef7
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-10 14:23:37 -0800

    display 4 coin names and info using CoinGecko API.
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import MarketService from './market.js';
+
+let promise = MarketService.sortCoin();
+promise.then(function(response) {
+  
+  const body = JSON.parse(response);
+  console.log(Object.entries(body));
+  for (const [key, value] of Object.entries(body)){
+    const nm = key.toUpperCase();
+    const price = JSON.stringify(value.usd);
+    const change= JSON.stringify(value.usd_24h_change);
+    const vol= JSON.stringify(value.usd_24h_vol);
+    const cap= JSON.stringify(value.usd_market_cap);
+    $("#sorted").append(`<p>${nm} Price: $${price} | 24Hour Change:$${change} | Volume:$${vol} | Market Cap:$${cap} <br></p>`);
+  }
+});
---
+++ src/market.js
@@ -0,0 +1,17 @@
+export default class MarketService {
+  static sortCoin() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Clitecoin%2Cethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
